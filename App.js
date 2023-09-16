f33e0056ce20d0d3cbcdc84e7dbd6dd438d08ee7
ali ouchagour <ali.ouchagour01@gmail.com>
2023-09-16 13:22:45 +0100

    building the UI design  of the game

--- App.js
+++ App.js
@@ -1,21 +1,103 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {React,useState} from 'react';
+import { StyleSheet, Text, View,SafeAreaView,TouchableOpacity } from 'react-native';
+import { RadioButton } from 'react-native-paper';
 
 export default function App() {
+
+
+
+const [value, setValue] = useState('first');
+const handleSubmit=(value)=>{
+ 
+  alert(value)
+  
+}
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.body}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+
+      <Text style={styles.question} >Open up App.js to start working on your app!</Text>
+
+
+      </View>
+      <View style={styles.answer}>
+
+      <RadioButton.Group onPress={()=>handleSubmit(value)} onValueChange={value => setValue(value)} value={value}>
+      <RadioButton.Item label="First item" value="first" />
+      <RadioButton.Item label="Second item" value="second" />
+    </RadioButton.Group>
+
+
+
+      </View>
+    
+      <TouchableOpacity style={styles.submit}  onPress={() =>{handleSubmit}}>
+        <Text style={styles.play}>Submit</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#22223b',
+  },
+  container: {
+    width: '100%',
+    height: 500,
+    backgroundColor: 'white',
+    position: 'relative',
+  },
+  content: {
+    padding: 50,
+  },
+  question: {
+    fontSize: "20%",
+  },
+  answer: {
+    fontSize: 24,
+    marginBottom: 20,
+    borderWidth: 2,
+    borderColor: "#20232a",  
+  },
+  submit: {
+    backgroundColor: 'rebeccapurple',
+    color: 'white',
+    border: 'none',
+    width: '100%',
+    height: 50,
+    position: 'absolute',
+    bottom: 0,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  result: {
+    display: 'none',
+    padding: 50,
+    textAlign: 'center',
+  },
+  resultText: {
+    fontSize: 50,
+  },
+  correct: {
+    color: 'green',
+  },
+  wrong: {
+    color: 'red',
+  },
+  play: {
+    color: 'white',
+    backgroundColor: 'rebeccapurple',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    cursor: 'pointer',
+    border: 'none',
+  },
+
 });
